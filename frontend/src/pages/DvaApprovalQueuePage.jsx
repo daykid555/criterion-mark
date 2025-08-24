479b4dcbd1cf871d9b591d613a26a4c12094b3b9
daykid555 <daykid555@gmail.com>
2025-08-24 13:33:58 +0100

    fix: CRITICAL - Remove invalid text from DvaApprovalQueuePage causing build failure

--- frontend/src/pages/DvaApprovalQueuePage.jsx
+++ frontend/src/pages/DvaApprovalQueuePage.jsx
@@ -11,21 +11,4 @@ function DvaApprovalQueuePage() {
   );
 }
 
-export default DvaApprovalQueuePage;```
-
-#### New File: `DvaHistoryPage.jsx`
-```jsx
-// frontend/src/pages/DvaHistoryPage.jsx
-import React from 'react';
-import DvaHistory from '../components/DvaHistory';
-
-function DvaHistoryPage() {
-  return (
-    <div className="p-4 sm:p-6 lg:p-8">
-      <h1 className="text-3xl sm:text-4xl font-bold text-white mb-8 drop-shadow-lg">Action History</h1>
-      <DvaHistory />
-    </div>
-  );
-}
-
-export default DvaHistoryPage;
+export default DvaApprovalQueuePage;
